e1c52e12cd637d230be152f0f6d5a692e40cac4e
ksd1 <1614545312@qq.com>
2023-11-02 17:57:01 +0800

    对比放置和序列化下载算法

--- draw.py
+++ draw.py
@@ -17,28 +17,38 @@ def prepare_draw_cdf(data1, data2):
     data = np.array([prob, data1, data2]).T
     np_to_csv(data, result_path + "lat_cdf.csv")
 
-def draw_cdf(filename = "lat_cdf.csv"):
+
+def prepare_draw_cdf_s(data):
+    assert len(data) >= 2, "unmatched data length"
+    ds = []
+    prob = []
+    inc = 100. / len(data[0])
+    k = inc
+    for i in range(len(data[0])):
+        prob.append(k)
+        k += inc
+    ds.append(prob)
+    for i in data:
+        ds.append(sorted(i))
+    ds = np.array(ds).T
+    np_to_csv(ds, result_path + "lat_cdf.csv")
+
+
+def draw_cdf(n, label, filename = "lat_cdf.csv"):
     #读取CSV文件
     data = pd.read_csv(result_path + filename)
 
-    #提取延迟列
-    delays = data.iloc[:,0]
-    dep_delays = data.iloc[:,1]
-    kube_delays = data.iloc[:,2]
-
-
-    #绘制CDF曲线
-    plt.plot(dep_delays, delays, label='LASA')
-    plt.plot(kube_delays, delays, label='RS')
+    prob = data.iloc[:,0]
+    for i in range(n):
+        delays = data.iloc[:,i+1]
+        plt.plot(delays, prob, label=label[i])
 
     #设置图表属性
     plt.xlabel('Latency')
     plt.ylabel('CDF') 
     # plt.xlim(0,80000)
     plt.legend()
-
-
     plt.show()
 
 if __name__ == "__main__":
-    draw_cdf("lat_cdf.csv")
+    draw_cdf(2, ["RS", "RS+UPL"], "lat_cdf.csv")
--- experiment.py
+++ experiment.py
@@ -4,9 +4,9 @@ from cluster import Cluster
 from analysis import Analysis
 import draw
 
-def exp_one(data, place_algo="LASA", seq_algo="GLSA"):
+def exp_one(data, requests, place_algo, seq_algo):
     # 准备环境
-    requests = data.new_requests()
+    # requests = data.new_requests()
     cluster = Cluster()
 
     # 调度
@@ -25,13 +25,22 @@ def exp_one(data, place_algo="LASA", seq_algo="GLSA"):
     # draw.draw_cdf("lat_cdf.csv")
 
 def exp(data):
+    requests = data.new_requests()
     latency = {}
+    latency_data = []
+    latency_name = []
 
     place_algo = ["LASA", "RS"]
     seq_algo = ["GLSA", "FCFS"]
-    for place,seq in zip(place_algo, seq_algo):
-        latency[place+"-"+seq] = exp_one(data, place, seq)
-    draw.prepare_draw_cdf(latency["LASA-GLSA"], latency["RS-FCFS"])
-    draw.draw_cdf()
+    for place in place_algo:
+        for seq in seq_algo:
+            name = place+"-"+seq
+            lat = exp_one(data, requests, place, seq)
+            latency[name] = lat
+            latency_data.append(lat)
+            latency_name.append(name)
+    
+    draw.prepare_draw_cdf_s(latency_data)
+    draw.draw_cdf(len(place_algo)*len(seq_algo), latency_name, "lat_cdf.csv")
 
 
